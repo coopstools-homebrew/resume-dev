6017d037008694084d8655b438cb7b220abe5be1
James Roger Cooper, PhD <James@coopstools.com>
2025-03-30 00:25:32 -0400

    Merging to main, which is now staging for the real thing. (#1)
    * Added in login page to site
    
    * Added in user info on auth
    
    * Updated user info display
    
    * pulled auth values into environment variables

---
+++ src/vue/sections/extra/LoginSection.vue
@@ -0,0 +1,118 @@
+<template>
+    <SectionTemplate :section-data="props.sectionData">
+        <h1>GitHub</h1>
+        <div v-if="loadingComplete">
+            <button v-if="!isAuthenticated" class="btn btn-primary btn-lg" @click="redirectToGitHub">
+                <i class="fa-brands fa-github me-2"></i>Login with GitHub
+            </button>
+            <div v-else class="mt-4 d-flex align-items-center">
+                <img :src="avatarUrl" alt="GitHub Avatar" class="rounded-circle" style="width: 24px; height: 24px;" />
+                <span class="ms-2">{{ orgName }}</span>
+            </div>
+        </div>
+    </SectionTemplate>
+</template>
+
+<script setup>
+import SectionTemplate from "../_templates/SectionTemplate.vue"
+import {computed, ref, onMounted} from "vue"
+import {useData} from "../../../composables/data.js"
+import {useNavigation} from "../../../composables/navigation.js"
+
+const data = useData()
+const navigation = useNavigation()
+const avatarUrl = ref(null)
+const loadingComplete = ref(false)
+const isAuthenticated = ref(false)
+const orgName = ref('')
+
+const ctApiUrl = import.meta.env.VITE_CT_API_URL
+const ctRedirectUrl = ctApiUrl + import.meta.env.VITE_CT_GITHUB_REDIRECT_PATH
+const ctUserDataUrl = ctApiUrl + import.meta.env.VITE_CT_USER_DATA_PATH
+
+
+const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID
+
+/**
+ * @property {Object} sectionData
+ */
+const props = defineProps({
+    sectionData: Object
+})
+
+/**
+ * @type {ComputedRef<String>}
+ */
+const loginTitle = computed(() => {
+    return props.sectionData.content['locales']['title']
+})
+
+const fetchUserData = async () => {
+    try {
+        const response = await fetch(ctUserDataUrl, {
+            credentials: 'include'
+        })
+
+        if (response.status >= 500) {
+            throw new Error(`Server error: ${response.status}`)
+        }
+
+        if (response.status === 400 || response.status === 401) {
+            isAuthenticated.value = false
+            return
+        }
+
+        if (response.status < 400) {
+            const userData = await response.json()
+            if (userData.avatar_url) {
+                avatarUrl.value = userData.avatar_url
+                orgName.value = userData.org_name || ''
+                isAuthenticated.value = true
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching user data:', error)
+        throw error // Re-throw server errors
+    } finally {
+        loadingComplete.value = true
+    }
+}
+
+onMounted(() => {
+    fetchUserData()
+})
+
+const redirectToGitHub = () => {
+    const githubConfig = props.sectionData.content.items.github
+    const params = new URLSearchParams({
+        client_id: githubClientId,
+        redirect_uri: ctRedirectUrl,
+        scope: githubConfig.scopes.join(' ')
+    })
+    window.location.href = `${githubConfig.url}?${params.toString()}`
+}
+</script>
+
+<style lang="scss" scoped>
+@import "/src/scss/_theming.scss";
+
+.login-title {
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+    font-weight: bold;
+}
+
+.solid-divider {
+    @include media-breakpoint-up($navigation-sidebar-breakpoint) {
+        display: none;
+    }
+}
+
+.info-list {
+    @include generate-dynamic-styles-with-hash((
+        xxxl: (margin-bottom: 2.5rem),
+        lg: (margin-bottom: 2rem),
+        md: (margin-bottom: 1.2rem)
+    ))
+}
+</style>
